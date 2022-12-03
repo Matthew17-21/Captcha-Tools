b937907b7558d9e7ceda527f7e7b13f2fbe83bcb
Matthew <matthewlugo746@gmail.com>
2022-12-03 18:45:32 -0500

    Update __init__.py

--- captchatools/__init__.py
+++ captchatools/__init__.py
@@ -16,7 +16,27 @@ Sites:
 2 = Anticaptcha
 3 = 2captcha
 '''
-from .harvesters import captcha_harvesters
-from .exceptions import(
-    WrongAPIKeyException, NoBalanceException, WrongSitekeyException, NoHarvesterException
+__version__ = "2.0.0"
+__author__ = "Matthew17-21"
+__license__ = "MIT"
+
+from .harvester import Harvester
+from .twocap import Twocap
+from .exceptions import (
+    NoHarvesterException
 )
+
+def new_harvester(**kwargs) -> Harvester:
+    '''
+    Returns a new captcha harvester with the specified config
+    '''
+    site = kwargs.get("solving_site",None)
+    if site is None:
+        raise NoHarvesterException(
+            "No captcha harvester was selected. Double check you entered the site name correctly "
+            +
+            "|| Double check the site id is type int"
+        )
+
+    if site == 3 or str(site).lower() == "2captcha":
+        return Twocap(**kwargs)

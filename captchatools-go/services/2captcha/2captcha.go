51db79176811febd46dd25a74b73d0f1ca8b9aac
Matthew Lugo <matthewlugo746@gmail.com>
2025-03-17 15:23:06 -0400

    GetToken functionality

--- captchatools-go/services/2captcha/2captcha.go
+++ captchatools-go/services/2captcha/2captcha.go
@@ -2,7 +2,6 @@ package twocaptcha
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/Matthew17-21/Captcha-Tools/captchatools-go/harvester"
 )
@@ -14,6 +13,20 @@ const (
 
 type Twocaptcha struct {
 	harvester.Config
+}
+
+func New(c harvester.Config) *Twocaptcha {
+	return &Twocaptcha{c}
+}
+
+// GetToken obtains a captcha token from 2Captcha with default context
+func (t Twocaptcha) GetToken(opts ...harvester.TokenOption) (harvester.CaptchaAnswer, error) {
+	return t.GetTokenWithContext(context.Background(), opts...)
+}
+
+// GetTokenWithContext obtains a captcha token from 2Captcha with a custom context
+func (t Twocaptcha) GetTokenWithContext(ctx context.Context, opts ...harvester.TokenOption) (harvester.CaptchaAnswer, error) {
+	return t.getToken(ctx, baseURL, defaultPollingTimeout, opts...)
 }
 
 // Attempt to get the balance from the API
